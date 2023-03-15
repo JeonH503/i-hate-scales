39beae13c3353a30a6221eab53575ca33c4a6770
jeon <wjs4222@naver.com>
2023-03-15 23:01:50 +0900

    프렛보드 프렛 별 하얀점 추가, 음계 추가

--- pages/scales.tsx
+++ pages/scales.tsx
@@ -28,13 +28,40 @@ const String = styled.div`
         width:100%;
         height:5px;
         background:#A59481;
+        z-index:2;
     }
 `
 
-const Flet = styled.div`
+const Flet = styled.div<{direction?:string}>`
     width:4.6%;
     height:100%;
     border-right: 4px solid #958269;
+    display:flex;
+    justify-content:center;
+
+    ${({direction}) => direction ? `
+    &:before { 
+        content:''; 
+        position:absolute; 
+        width:20px; 
+        height:20px; 
+        border-radius:20px; 
+        background:white;
+        ${direction === 'down' ? "bottom:-10px;" : "bottom: 8px;" }
+        z-index:1;
+    }` : ""}
+
+    &:after {
+        text-align:center;
+        content:'E'; 
+        position:absolute; 
+        width:20px; 
+        height:20px; 
+        border-radius:20px; 
+        background:yellow;
+        bottom: 8px;
+        z-index:3;
+    }
 ` //각각의 프렛
 const FletNumber = styled.div`` //몇번째 프렛인지 표사
 const OpenString = styled.div`` //개방현 표시
@@ -79,6 +106,7 @@ export default function scales() {
                         <Flet/>
                         <Flet/>
                         <Flet/>
+                        <Flet direction="up"/>
                         <Flet/>
                         <Flet/>
                         <Flet/>
@@ -88,7 +116,29 @@ export default function scales() {
                         <Flet/>
                         <Flet/>
                         <Flet/>
+                    </String>
+                    <String>
                         <Flet/>
+                        <Flet/>
+                        <Flet direction="down"/>
+                        <Flet/>
+                        <Flet direction="down"/>
+                        <Flet/>
+                        <Flet direction="down"/>
+                        <Flet/>
+                        <Flet direction="down"/>
+                        <Flet/>
+                        <Flet/>
+                        <Flet/>
+                        <Flet/>
+                        <Flet/>
+                        <Flet direction="down"/>
+                        <Flet/>
+                        <Flet direction="down"/>
+                        <Flet/>
+                        <Flet direction="down"/>
+                        <Flet/>
+                        <Flet direction="down"/>
                     </String>
                     <String>
                         <Flet/>
@@ -125,30 +175,7 @@ export default function scales() {
                         <Flet/>
                         <Flet/>
                         <Flet/>
-                        <Flet/>
-                        <Flet/>
-                        <Flet/>
-                        <Flet/>
-                        <Flet/>
-                        <Flet/>
-                        <Flet/>
-                        <Flet/>
-                        <Flet/>
-                        <Flet/>
-                    </String>
-                    <String>
-                        <Flet/>
-                        <Flet/>
-                        <Flet/>
-                        <Flet/>
-                        <Flet/>
-                        <Flet/>
-                        <Flet/>
-                        <Flet/>
-                        <Flet/>
-                        <Flet/>
-                        <Flet/>
-                        <Flet/>
+                        <Flet direction="up"/>
                         <Flet/>
                         <Flet/>
                         <Flet/>
