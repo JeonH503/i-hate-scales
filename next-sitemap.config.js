ffa47da5d8fba5b32d54a82494edfcbee7d30832
jeon <wjs4222@naver.com>
2023-03-29 22:52:30 +0900

    siteurl 주소 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-    siteUrl: 'https://i-hate-scales',
+    siteUrl: 'https://i-hate-scales.online',
     generateRobotsTxt: true,
     sitemapSize: 7000,
     changefreq: 'daily',
